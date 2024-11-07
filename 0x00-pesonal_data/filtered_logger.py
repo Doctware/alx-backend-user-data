2b166c947cc7db41f90f9a3aca10b793e8c41676
Doctware <yusclever9@gmail.com>
2024-11-07 14:22:11 +0100

    flitterd logger

---
+++ 0x00-pesonal_data/filtered_logger.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" this module contains the filtered logger function """
+import re
+from typing import List
+
+
+def filter_datum(field: List[str], redaction: str, message: str,
+                separator: str) -> str:
+    """
+    this fun=ctions returns the log message obfuscated
+    Args:
+        fields: a list of strings repesenting all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which caharacter is separating
+        all fields in the log line (message)
+
+    Then use a regex to replace all occurances of certain feild value
+    """
+    pattern = f"({'|'.join(fields)})=([^ {separator}]+)"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
