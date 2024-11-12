f0b37a1fcfe995c79a69fed0af9d59e8cfb62b04
Doctware <yusclever9@gmail.com>
2024-11-12 16:35:13 +0100

    implementing error 401

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -28,4 +28,6 @@ def stats() -> str:
 
 @app_views.route("/unauthorized/", methods=["GET"], strict_slashes=False)
 def unauthorized() -> str:
+    """ if unauthrized access
+    abort with error 401 """
     abort(401)
