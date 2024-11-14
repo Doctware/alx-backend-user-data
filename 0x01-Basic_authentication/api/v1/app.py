1f87cea3052eca4d3c45cb1f396fda634936c62d
Doctware <yusclever9@gmail.com>
2024-11-14 11:18:09 +0100

    Imlementitng forbidden error

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,6 +27,12 @@ def unautorized_error(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def forbbiden_error(error):
+    """ Forbidden handler """
+    return jsonify({"error": "Forbidden"})
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
