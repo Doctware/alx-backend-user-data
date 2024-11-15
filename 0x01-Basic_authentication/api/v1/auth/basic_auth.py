6e8b2378bf968ebf5c2dc7c32b5840bf70bcd210
Doctware <yusclever9@gmail.com>
2024-11-15 21:31:22 +0100

    The basic authenticator module

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" this module implement basic authentication """
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """ the class Basiauth thats inherit from Auth """
+    pass
