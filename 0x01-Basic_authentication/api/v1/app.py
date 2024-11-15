860a0678c58e4b1e51a102a815e04db4cefcc2ae
Doctware <yusclever9@gmail.com>
2024-11-15 21:38:26 +0100

    now implementing basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,7 +19,7 @@ AUTH_TYPE = os.getenv('AUTH_TYPE')
 if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == 'basi_auth':
+elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
