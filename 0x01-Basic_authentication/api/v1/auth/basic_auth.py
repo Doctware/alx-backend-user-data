55c1383e4fda08e6c75879615750512b333950de
Doctware <yusclever9@gmail.com>
2024-11-17 17:58:54 +0100

    Implementing base 64 exraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,19 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ the class Basiauth thats inherit from Auth """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+            ) -> str:
+        """
+        this method returns base64 part of the
+        Authorization header for basic authentication
+        """
+        if not authorization_header or not\
+            isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[6:]
