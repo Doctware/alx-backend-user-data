8e58b3887fe48920baaeea7b3871fd4989155116
Doctware <yusclever9@gmail.com>
2024-11-17 19:06:46 +0100

    o writing

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ this module implement basic authentication """
 from api.v1.auth.auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """ the class Basiauth thats inherit from Auth """
@@ -20,4 +20,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
 
-        return authorization_header[6:]
+        return iauthorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+            ) -> str:
+        """
+        this method return the decoded value of Base64 string
+        """
+        if not authorization_header or not\
+                isinstance(authorization_header, str):
+            return None
+
+        try:
+            decode_byte = base64.b64decode(authorization-header)
